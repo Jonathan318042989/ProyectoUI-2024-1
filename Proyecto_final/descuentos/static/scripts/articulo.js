09b68c35daee68058883db370359cdfbc30aa76e
StrayMonster <alan_lucero@ciencias.unam.mx>
2023-12-08 19:43:27 -0600

    Adición de la sección de descuentos.
    -Se añadió la sección de descuentos.
    Ya esta el esqueleto, se tiene que visualizar la consola, para ver el correcto funcionamiento de la misma. Solo falta un pequeño detalle de que se actualice el precio del descuento, una ves que se aplique el descuento.

---
+++ Proyecto_final/descuentos/static/scripts/articulo.js
@@ -0,0 +1,59 @@
+/** Archivo en el que se encuentran los artículos disponibles a la venta. Si desea añadir un artículo copie el siguiente código.
+ *     {
+        id: número que indica el id del artículo,
+        item: "Nombre artículo",
+        img: "Ubicación de la imágen, con el formato que se encuentra en el código no comentado",
+        precio: número que indica el precio del articulo,
+        precioD: 0,
+        tipo: "Que tipo de artículo es",
+    },
+ *En este caso si aún tiene continación. El último artículo que añada, incluirlo sin la coma final del {}
+ *Igualmente se le puede añadir más información que se requiera.
+ * En el archivo base o version 1 solo se tienen 5 artículos.
+ */
+
+ 
+
+const articulos = [
+    {
+        id: 0,
+        item: "Playera Taylor Switf - Eras Tour",
+        img: "./images/foto_producto_1.jpg",
+        precio: 150,
+        precioD: 0,
+        tipo: "Ropa",
+    },
+    {
+        id: 1,
+        item: "Playera con diseño extravagante",
+        img: "./images/foto_producto_2.jpg",
+        precio: 300,
+        precioD: 0,
+        tipo: "Ropa",
+    },
+    {
+        id: 2,
+        item: "Playera 1989",
+        img: "./images/foto_producto_3.jpg",
+        precio: 1100,
+        precioD: 0,
+        tipo: "Ropa",
+    },
+    {
+        id: 3,
+        item: "Playera History",
+        img: "./images/foto_producto_4.jpg",
+        precio: 750,
+        precioD: 0,
+        tipo: "Ropa",
+    },
+    {
+        id: 4,
+        item: "Chamarra One Direction",
+        img: "./images/foto_producto_5.jpg",
+        precio: 1500,
+        precioD: 0,
+        tipo: "Ropa",
+    }
+
+]
---
+++ Proyecto_final/descuentos/static/scripts/cardinal.js
@@ -0,0 +1,84 @@
+/** Archivo que crea las tarjetas de presentación de los artículos existentes. Esto en la página principal */
+
+/** Variable que almacena el contenedor de los artículos */
+const contenedorProductos = document.getElementById("contenedor-productos");
+
+/** Función que crea las tarjetas de presentación de los artículos existentes. (Archivo articulo.js) */
+function crearPresentacionArticulos(productos){
+    productos.forEach(producto =>{
+        const nuevoArticulo = document.createElement("table");
+        nuevoArticulo.classList = "tarjeta-articulo" ;
+        nuevoArticulo.innerHTML = `
+        <tr id= "headeD">
+            <th>Nombre del artículo</th>
+            <th> ID </th>
+            <th> Imágen </th>
+            <th> Tipo </th>
+            <th> Precio Orig</th>
+            <th> Precio Desc</th>
+        </tr>
+        <tr id="subheaderD">
+            <td> ${producto.item} </td>
+            <td> ${producto.id} </td>
+            <td> <img class="card-img-top" src=${producto.img}> </td>
+            <td> ${producto.tipo} </td>
+            <td> ${producto.precio}</td>
+            <td> ${producto.precioD} </td>
+        </tr>
+        `
+        contenedorProductos.appendChild(nuevoArticulo);
+    });
+}
+/** Se ejecuta la función al cargar la página */
+crearPresentacionArticulos(articulos);
+
+
+
+const boton1 = document.getElementById("10");
+const boton2 = document.getElementById("20");
+const boton3 = document.getElementById("30");
+const boton4 = document.getElementById("40");
+const boton5 = document.getElementById("50");
+
+boton1.addEventListener("click", descuentosApi());
+
+function setprecioDesc(valor){
+    precioD = valor;
+}
+
+function descuentosApi(param){
+    if(param === 10){
+        descontar(param, articulos);
+        console.log("10% de descuento aplicado");
+    } else if(param === 20){
+        descontar(param, articulos);
+        console.log("20% de descuento aplicado");
+    } else if(param === 30){
+        descontar(param, articulos);
+        console.log("30% de descuento aplicado");
+    } else if(param === 40){
+        descontar(param, articulos);
+        console.log("40% de descuento aplicado");
+    } else if(param === 50){
+        descontar(param, articulos);
+        console.log("50% de descuento aplicado");
+    } else {
+        console.log("No se ha aplicado ningun desucento");
+    }
+}
+
+var guardable = 0;
+function descontar(param, productos){
+    var i = 0;
+    productos.forEach(producto => {
+        console.log("Articulo " + i);
+        i = i + 1;
+        guardable = producto.precio / 100;
+        guardable = guardable * param;
+        console.log("Total a descontar: " + guardable);
+        guardable = producto.precio - guardable;
+        setprecioDesc(guardable);
+        console.log("Precio con descuento del " + param + "%: " + precioD)
+    })
+
+}
